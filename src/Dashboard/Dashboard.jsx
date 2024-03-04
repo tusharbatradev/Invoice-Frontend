0578a94df7d1321b7385aa72bd586ebbbdec1f3e
Tushar Batra <tusharbatra08@gmail.com>
2024-03-04 12:03:40 +0530

    Dashboard Styling Done.

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -3,30 +3,38 @@ import MenuList from "./MenuList";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import ProfileButton from "./ProfileButton";
+
 function Dashboard() {
-  const isUserLoggedIn = useSelector((state) => state.auth.isUserLoggedIn);
-  console.log("isUserLoggedIn", isUserLoggedIn);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (isUserLoggedIn === false) {
-      navigate("/Login");
-    }
-  }, []);
-  return (
-    <Stack padding={"9px"} height={"97vh"}>
-      <Box
-        sx={{
-          backgroundColor: "#0c1526",
-          width: "17.5%",
-          height: "100%",
-          borderRadius: "8px",
-          padding: "12px",
-        }}
-      >
-        <MenuList />
-      </Box>
-    </Stack>
-  );
+	const isUserLoggedIn = useSelector((state) => state.auth.isUserLoggedIn);
+	console.log("isUserLoggedIn", isUserLoggedIn);
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (isUserLoggedIn === false) {
+			navigate("/Login");
+		}
+	}, []);
+	return (
+		<Stack padding={"10px 10px 0px 10px"} height={"97vh"} sx={{ backgroundColor: '#eeeff1' }}>
+			<Box
+				sx={{
+					backgroundColor: "#0c1526",
+					width: "17.5%",
+					height: "100%",
+					borderRadius: "8px",	
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'space-between'
+				}}
+			>
+				<MenuList />
+				<Box sx={{ padding: '0px' }}>
+					<ProfileButton />
+				</Box>
+			</Box>
+
+		</Stack>
+	);
 }
 
 export default Dashboard;
--- src/assets/CustomIcons/Icons.jsx
+++ src/assets/CustomIcons/Icons.jsx
@@ -1,6 +1,11 @@
 import PersonIcon from '@mui/icons-material/Person';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import DescriptionIcon from '@mui/icons-material/Description';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 export const UserIcon=()=> {
     return(<>
@@ -19,3 +24,28 @@ export const CloseEyeIcon=()=> {
         <VisibilityOffIcon />
     </>)
 }
+
+export const DashboardIcon=()=> {
+    return(<>
+        <SpaceDashboardIcon />
+    </>)
+}
+
+export const ProductIcon=()=> {
+    return(<>
+        <Inventory2Icon />
+    </>)
+}
+
+export const InvoiceIcon=()=> {
+    return(<>
+        <DescriptionIcon />
+    </>)
+}
+
+export const ProfileIcon=()=> {
+    return(<>
+        <AccountCircleIcon sx={{ fontSize: '30px' }} />
+    </>)
+}
+
