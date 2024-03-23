1eeb5ad4bb1bc29ccbc4b73b0fb27adc2ab84cd2
Aman <avman1998@gmail.com>
2024-03-23 18:20:46 +0530

    product add done

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Divider, Stack } from "@mui/material";
 import ProductHeader from "./ProductHeader";
 import Table from "./ProductTable";
 
 function Product() {
-    const [reload,setReload]=useState(false);
-    return (
-        <Stack spacing={'8px'} sx={{
-            backgroundColor: '#fff',
-            width: '100%',
-            padding: '14px',
-            borderRadius: '8px'
-        }}>
-
-            <ProductHeader setReload={setReload}/>
-            <Divider />
-            <Table reload={reload}/>
-        </Stack>
-    );
+  const [reload, setReload] = useState(false);
+  return (
+    <Stack
+      spacing={"8px"}
+      sx={{
+        backgroundColor: "#fff",
+        width: "100%",
+        padding: "14px",
+        borderRadius: "8px",
+      }}
+    >
+      <ProductHeader setReload={setReload} />
+      <Divider />
+      <Table reload={reload} setReload={setReload} />
+    </Stack>
+  );
 }
 
 export default Product;
--- src/Product/ProductTable.jsx
+++ src/Product/ProductTable.jsx
@@ -1,249 +1,271 @@
-import { useEffect, useState } from 'react';
-import { Box, Button, IconButton, Skeleton, Snackbar, Typography } from '@mui/material';
-import { CrossCancelIcon, EditedIcon, ProductIcon, SaveIcon } from '../assets/CustomIcons/Icons';
-import { Stack } from '@mui/system';
-import axios from 'axios';
-import { DeleteBasketIcon } from '../assets/CustomIcons/Icons';
+import { useEffect, useState } from "react";
 import {
-    GridRowModes,
-    DataGrid,
-    GridToolbarContainer,
-    GridActionsCellItem,
-    GridRowEditStopReasons,
-} from '@mui/x-data-grid';
+  Box,
+  Button,
+  IconButton,
+  Skeleton,
+  Snackbar,
+  Typography,
+} from "@mui/material";
+import {
+  CrossCancelIcon,
+  EditedIcon,
+  ProductIcon,
+  SaveIcon,
+} from "../assets/CustomIcons/Icons";
+import { Stack } from "@mui/system";
+import axios from "axios";
+import { DeleteBasketIcon } from "../assets/CustomIcons/Icons";
+import {
+  GridRowModes,
+  DataGrid,
+  GridToolbarContainer,
+  GridActionsCellItem,
+  GridRowEditStopReasons,
+} from "@mui/x-data-grid";
 
-
-export default function Table({ reload }) {
-    const columns = [
-        {
-            field: 'productName',
-            headerName: 'Product name',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'availability',
-            headerName: 'Status',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'actions',
-            type: 'actions',
-            headerName: 'Actions',
-            width: 100,
-            cellClassName: 'actions',
-            getActions: (params) => {
-                const { id } = params.row;
-                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-                if (isInEditMode) {
-                    return [
-                        <GridActionsCellItem
-                            icon={<SaveIcon />}
-                            label="Save"
-                            sx={{
-                                color: 'primary.main',
-                            }}
-                            onClick={() => handleSaveClick(params)}
-                        />,
-                        <GridActionsCellItem
-                            icon={<CrossCancelIcon />}
-                            label="Cancel"
-                            className="textPrimary"
-                            onClick={() => handleCancelClick(params)}
-                            color="inherit"
-                        />,
-                    ];
-                }
-
-                return [
-                    <GridActionsCellItem
-                        icon={<EditedIcon />}
-                        label="Edit"
-                        className="textPrimary"
-                        onClick={() => handleEditClick(params)}
-                        color="inherit"
-                    />,
-                    <GridActionsCellItem
-                        icon={<DeleteBasketIcon />}
-                        label="Delete"
-                        onClick={() => handleDelete(params)}
-                        color="inherit"
-                    />,
-                ];
-            },
-        },
-    ];
-
-    const [rowModesModel, setRowModesModel] = useState({});
-    const [rows, setRows] = useState([]);
-    const [snackBar, setSnackBar] = useState(false);
-    const [snackBarMessage, setSnackBarMessage] = useState("");
-    const [snackBarColor, setSnackBarColor] = useState("");
-
-    // Edit click handler
-    const handleEditClick = (params) => {
+export default function Table({ reload, setReload }) {
+  const columns = [
+    {
+      field: "productName",
+      headerName: "Product name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "availability",
+      headerName: "Status",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: (params) => {
         const { id } = params.row;
-        setSnackBarMessage("Please Edit the Product")
-        setSnackBar(true)
-        setSnackBarColor("green")
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-    };
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
 
-    // Save click handler
-    const handleSaveClick = async (params) => {
-        console.log("Save params", params.row._id);
-        try {
-            // Convert the "availability" field to a Boolean value
-            const availability = params.row.availability === "Available";
-
-            const updatedProduct = await axios.patch(
-                `http://localhost:3001/product/${params.row._id}`,
-                { ...params.row, availability: availability },
-                {
-                    headers: {
-                        Authorization: localStorage.getItem("token")
-                    }
-                }
-            );
-            if (updatedProduct.status === 200) {
-                fetchProducts();
-                setSnackBarMessage("Product Updated");
-                setSnackBar(true);
-                setSnackBarColor("green");
-                console.log(updatedProduct)
-            }
-        } catch (error) {
-            console.error('Error updating document:', error);
-            setSnackBarMessage("Error updating product");
-            setSnackBar(true);
-            setSnackBarColor("red");
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={() => handleSaveClick(params)}
+            />,
+            <GridActionsCellItem
+              icon={<CrossCancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={() => handleCancelClick(params)}
+              color="inherit"
+            />,
+          ];
         }
-        setRowModesModel({ ...rowModesModel, [params.row._id]: { mode: GridRowModes.View } })
-    }
 
+        return [
+          <GridActionsCellItem
+            icon={<EditedIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={() => handleEditClick(params)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteBasketIcon />}
+            label="Delete"
+            onClick={() => handleDelete(params)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
 
-    // Cancel click handler
-    const handleCancelClick = (params) => {
-        const { id } = params.row;
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-    };
+  const [rowModesModel, setRowModesModel] = useState({});
+  const [rows, setRows] = useState([]);
+  const [snackBar, setSnackBar] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState("");
+  const [snackBarColor, setSnackBarColor] = useState("");
 
-    // Delete function for the product
-    const handleDelete = async (params) => {
-        console.log("delet params", params)
-        try {
-            const deletedProduct = await axios.delete(`http://localhost:3001/product/${params.row._id}`, {
-                headers: {
-                    Authorization: localStorage.getItem("token")
-                }
-            }
-            );
-            if (deletedProduct.status === 200) {
-                fetchProducts()
-            }
-            setSnackBarMessage("Product Deleted")
-            setSnackBar(true)
-            setSnackBarColor("red")
-        } catch (error) {
-            console.error('Error deleting document:', error);
+  // Edit click handler
+  const handleEditClick = (params) => {
+    const { id } = params.row;
+    setSnackBarMessage("Please Edit the Product");
+    setSnackBar(true);
+    setSnackBarColor("green");
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  // Save click handler
+  const handleSaveClick = async (params) => {
+    console.log("Save params", params.row._id);
+    try {
+      // Convert the "availability" field to a Boolean value
+      const availability = params.row.availability === "Available";
+
+      const updatedProduct = await axios.patch(
+        `http://localhost:3001/product/${params.row._id}`,
+        { ...params.row, availability: availability },
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
         }
-    };
-
-    // Fetch products from API
-    async function fetchProducts() {
-        try {
-            const response = await axios.get("http://localhost:3001/product", {
-                headers: {
-                    Authorization: localStorage.getItem("token")
-                }
-            });
-            if (response.status === 200) {
-                let count = 0;
-                const rowData = response.data.map((item) => {
-                    count++;
-                    return {
-                        ...item,
-                        id: count,
-                        availability: item.availability ? "Available" : "Not Available",
-                    };
-                });
-                setRows(rowData);
-            }
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        }
-    }
-
-    // Fetch products on component mount and reload change
-    useEffect(() => {
+      );
+      if (updatedProduct.status === 200) {
         fetchProducts();
-    }, [reload]);
+        setSnackBarMessage("Product Updated");
+        setSnackBar(true);
+        setSnackBarColor("green");
+        console.log(updatedProduct);
+      }
+    } catch (error) {
+      console.error("Error updating document:", error);
+      setSnackBarMessage("Error updating product");
+      setSnackBar(true);
+      setSnackBarColor("red");
+    }
+    setRowModesModel({
+      ...rowModesModel,
+      [params.row._id]: { mode: GridRowModes.View },
+    });
+  };
 
+  // Cancel click handler
+  const handleCancelClick = (params) => {
+    const { id } = params.row;
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
 
-    // Close SnackBar
-    const handleCloseSnackBar = () => {
-        setTimeout(() => {
-            setSnackBar(false);
-        }, 500);
-    };
+  // Delete function for the product
+  const handleDelete = async (params) => {
+    console.log("delet params", params);
+    try {
+      const deletedProduct = await axios.delete(
+        `http://localhost:3001/product/${params.row._id}`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+      if (deletedProduct.status === 200) {
+        fetchProducts();
+      }
+      setSnackBarMessage("Product Deleted");
+      setSnackBar(true);
+      setSnackBarColor("red");
+    } catch (error) {
+      console.error("Error deleting document:", error);
+    }
+  };
 
-    // Loading State
-    const [loading, setLoading] = useState(true);
+  // Fetch products from API
+  async function fetchProducts() {
+    try {
+      const response = await axios.get("http://localhost:3001/product", {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      });
+      if (response.status === 200) {
+        let count = 0;
+        const rowData = response.data.map((item) => {
+          count++;
+          return {
+            ...item,
+            id: count,
+            availability: item.availability ? "Available" : "Not Available",
+          };
+        });
+        setRows(rowData);
+        setReload(false);
+      }
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 600);
+  // Fetch products on component mount and reload change
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-        return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    if (reload === true) fetchProducts();
+  }, [reload]);
 
-    return (
-        <Stack paddingX={2}>
-            {loading ?
-                <Skeleton sx={{ height: '450px' }} /> :
-                <Box height={400} padding={2}>
-                    <DataGrid
-                        rows={rows}
-                        columns={columns}
-                        editMode="row"
-                        rowModesModel={rowModesModel}
-                        onRowEditStop={(params, event) => {
-                            if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-                                event.defaultMuiPrevented = true;
-                            }
-                        }}
-                        pageSize={5}
-                        checkboxSelection
-                        disableRowSelectionOnClick
-                    />
-                </Box>
-            }
+  // Close SnackBar
+  const handleCloseSnackBar = () => {
+    setTimeout(() => {
+      setSnackBar(false);
+    }, 500);
+  };
 
-            <Snackbar
-                ContentProps={{
-                    sx: {
-                        backgroundColor: snackBarColor
-                    }
-                }}
-                open={snackBar}
-                autoHideDuration={1000}
-                message={
-                    <Typography sx={{
-                        width: '100%',
-                        display: 'flex',
-                        alignItems: 'center',
-                        gap: '8px',
-                        fontFamily: 'Poppins'
-                    }}>
-                        {snackBarMessage}
-                    </Typography>
-                }
-                onClose={handleCloseSnackBar}
-                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-            />
-        </Stack>
-    );
+  // Loading State
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 600);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Stack paddingX={2}>
+      {loading ? (
+        <Skeleton sx={{ height: "450px" }} />
+      ) : (
+        <Box height={400} padding={2}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            editMode="row"
+            rowModesModel={rowModesModel}
+            onRowEditStop={(params, event) => {
+              if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+                event.defaultMuiPrevented = true;
+              }
+            }}
+            pageSize={5}
+            checkboxSelection
+            disableRowSelectionOnClick
+          />
+        </Box>
+      )}
+
+      <Snackbar
+        ContentProps={{
+          sx: {
+            backgroundColor: snackBarColor,
+          },
+        }}
+        open={snackBar}
+        autoHideDuration={1000}
+        message={
+          <Typography
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+              fontFamily: "Poppins",
+            }}
+          >
+            {snackBarMessage}
+          </Typography>
+        }
+        onClose={handleCloseSnackBar}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      />
+    </Stack>
+  );
 }
