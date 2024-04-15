49e1bb3ad9dfef3b3b175ec7590ab03c3b1d4856
Aman <avman1998@gmail.com>
2024-04-15 15:57:06 +0530

    add product working

--- src/Invoice/InvoicePage.jsx
+++ src/Invoice/InvoicePage.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import { Stack } from "@mui/material";
+import { Stack, Button, Typography } from "@mui/material";
 import CustomerDetails from "./CustomerDetails";
 import ProductDetails from "./ProductDetails";
 import OldProductDetails from "./OldProductDetails";
 import AmountDetails from "./AmountDetails";
 import axios from "axios";
-import { useSelector } from "react-redux";
+import { addProduct } from "../redux/Slices/invoiceSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function InvoicePage() {
   const products = useSelector((state) => state.invoice.products);
+  console.log("invoice products", products);
+  const dispatch = useDispatch();
+  const handleAddNewProduct = () => {
+    dispatch(
+      addProduct({
+        product: "",
+        productWeight: "",
+        productCost: "",
+        productQuantity: "",
+        makingCharges: "",
+        productId: "",
+        productAmount: "",
+      })
+    );
+  };
 
   console.log("invoice slice data", products);
   const [formData, setFormData] = useState({
@@ -60,7 +76,29 @@ function InvoicePage() {
   return (
     <Stack spacing={"-15px"}>
       <CustomerDetails formData={formData} setFormData={setFormData} />
-      <ProductDetails formData={formData} setFormData={setFormData} />
+      <Typography lineHeight={"25px"} fontFamily={"Poppins"} fontWeight={500}>
+        Product Details{" "}
+        <span style={{ color: "red", alignSelf: "center" }}>*</span>
+      </Typography>
+      {products.map((prod) => (
+        <ProductDetails formData={formData} setFormData={setFormData} />
+      ))}
+      <Button
+        onClick={handleAddNewProduct}
+        sx={{
+          backgroundColor: "#0c1526",
+          height: "35px",
+          width: "100%",
+          fontFamily: "Poppins",
+          marginTop: "16px",
+          "&:hover": {
+            backgroundColor: "#1d2659",
+          },
+        }}
+        variant="contained"
+      >
+        Add products +
+      </Button>
       <OldProductDetails />
       <AmountDetails
         formData={formData}
--- src/Invoice/ProductDetails.jsx
+++ src/Invoice/ProductDetails.jsx
@@ -1,166 +1,179 @@
-import { Box, Stack, TextField, Typography, Grid, Button, Skeleton } from "@mui/material";
+import {
+  Box,
+  Stack,
+  TextField,
+  Typography,
+  Grid,
+  Button,
+  Skeleton,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 // import { UseSelector, useDispatch } from "react-redux";
 
 function ProductDetails({ formData, setFormData }) {
-
-    const handleChange = (index, fieldName, value) => {
-        const updatedProducts = [...formData.product];
-        updatedProducts[index] = {
-            ...updatedProducts[index],
-            [fieldName]: value
-        };
-
-        setFormData({
-            ...formData,
-            product: updatedProducts
-        });
+  const handleChange = (index, fieldName, value) => {
+    const updatedProducts = [...formData.product];
+    updatedProducts[index] = {
+      ...updatedProducts[index],
+      [fieldName]: value,
     };
 
-    const [loading, setLoading] = useState(true);
+    setFormData({
+      ...formData,
+      product: updatedProducts,
+    });
+  };
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 600);
+  const [loading, setLoading] = useState(true);
 
-        return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 600);
 
-    return (
-        <Stack padding={'16px'} spacing={'4px'}>
-            {loading ? <Skeleton width={'140px'} height={'25px'} />
-                :
-                <Typography lineHeight={'25px'} fontFamily={'Poppins'} fontWeight={500}>
-                    Product Details <span style={{ color: 'red', alignSelf: 'center' }}>*</span>
-                </Typography>
-            }
+    return () => clearTimeout(timer);
+  }, []);
 
-            {loading ?
-                <Skeleton height={'185px'} />
-                :
-                formData.product.map((product, index) => (
-                    <Box key={index} sx={{
-                        padding: '16px',
-                        border: '1px solid #9fa8af',
-                        borderRadius: '12px',
-                        marginTop: '16px'
-                    }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined"
-                                    placeholder="Product Name"
-                                    value={product.productName}
-                                    onChange={(e) => handleChange(index, 'productName', e.target.value)}
-                                    sx={{
-                                        "& .MuiInputBase-root": {
-                                            height: "40px",
-                                            borderRadius: "8px",
-                                            "&.Mui-focused fieldset": {
-                                                borderColor: "#555555",
-                                            },
-                                        },
-                                    }}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined"
-                                    placeholder="Product Weight"
-                                    value={product.productWeight}
-                                    onChange={(e) => handleChange(index, 'productWeight', e.target.value)}
-                                    sx={{
-                                        "& .MuiInputBase-root": {
-                                            height: "40px",
-                                            borderRadius: "8px",
-                                            "&.Mui-focused fieldset": {
-                                                borderColor: "#555555",
-                                            },
-                                        },
-                                    }}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined"
-                                    placeholder="Product Cost"
-                                    value={product.productCost}
-                                    onChange={(e) => handleChange(index, 'productCost', e.target.value)}
-                                    sx={{
-                                        "& .MuiInputBase-root": {
-                                            height: "40px",
-                                            borderRadius: "8px",
-                                            "&.Mui-focused fieldset": {
-                                                borderColor: "#555555",
-                                            },
-                                        },
-                                    }}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined"
-                                    placeholder="Making Charges"
-                                    value={product.makingCharges}
-                                    onChange={(e) => handleChange(index, 'makingCharges', e.target.value)}
-                                    sx={{
-                                        "& .MuiInputBase-root": {
-                                            height: "40px",
-                                            borderRadius: "8px",
-                                            "&.Mui-focused fieldset": {
-                                                borderColor: "#555555",
-                                            },
-                                        },
-                                    }}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined"
-                                    placeholder="Product ID"
-                                    value={product.id}
-                                    onChange={(e) => handleChange(index, 'id', e.target.value)}
-                                    sx={{
-                                        "& .MuiInputBase-root": {
-                                            height: "40px",
-                                            borderRadius: "8px",
-                                            "&.Mui-focused fieldset": {
-                                                borderColor: "#555555",
-                                            },
-                                        },
-                                    }}
-                                />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField
-                                    fullWidth
-                                    variant="outlined"
-                                    placeholder="Product Quantity"
-                                    value={product.productQuantity}
-                                    onChange={(e) => handleChange(index, 'productQuantity', e.target.value)}
-                                    sx={{
-                                        "& .MuiInputBase-root": {
-                                            height: "40px",
-                                            borderRadius: "8px",
-                                            "&.Mui-focused fieldset": {
-                                                borderColor: "#555555",
-                                            },
-                                        },
-                                    }}
-                                />
-                            </Grid>
-                        </Grid>
-                    </Box>
-                ))
-            }
+  return (
+    <Stack padding={"16px"} spacing={"4px"}>
+      {loading ? (
+        <Skeleton height={"185px"} />
+      ) : (
+        formData.product.map((product, index) => (
+          <Box
+            key={index}
+            sx={{
+              padding: "16px",
+              border: "1px solid #9fa8af",
+              borderRadius: "12px",
+              marginTop: "16px",
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  placeholder="Product Name"
+                  value={product.productName}
+                  onChange={(e) =>
+                    handleChange(index, "productName", e.target.value)
+                  }
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      height: "40px",
+                      borderRadius: "8px",
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#555555",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  placeholder="Product Weight"
+                  value={product.productWeight}
+                  onChange={(e) =>
+                    handleChange(index, "productWeight", e.target.value)
+                  }
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      height: "40px",
+                      borderRadius: "8px",
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#555555",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  placeholder="Product Cost"
+                  value={product.productCost}
+                  onChange={(e) =>
+                    handleChange(index, "productCost", e.target.value)
+                  }
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      height: "40px",
+                      borderRadius: "8px",
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#555555",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  placeholder="Making Charges"
+                  value={product.makingCharges}
+                  onChange={(e) =>
+                    handleChange(index, "makingCharges", e.target.value)
+                  }
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      height: "40px",
+                      borderRadius: "8px",
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#555555",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  placeholder="Product ID"
+                  value={product.id}
+                  onChange={(e) => handleChange(index, "id", e.target.value)}
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      height: "40px",
+                      borderRadius: "8px",
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#555555",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  placeholder="Product Quantity"
+                  value={product.productQuantity}
+                  onChange={(e) =>
+                    handleChange(index, "productQuantity", e.target.value)
+                  }
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      height: "40px",
+                      borderRadius: "8px",
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#555555",
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </Box>
+        ))
+      )}
 
-            {loading ?
+      {/* {loading ?
                 <Skeleton height={'35px'} width={'100%'} />
                 :
                 <Button
@@ -178,10 +191,9 @@ function ProductDetails({ formData, setFormData }) {
                 >
                     Add Product +
                 </Button>
-            }
-
-        </Stack>
-    );
+            } */}
+    </Stack>
+  );
 }
 
 export default ProductDetails;
